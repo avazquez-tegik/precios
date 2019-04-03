b093487796873976bb5e0c2e242798e1439e4f20
ccastillo_tegik <ccastillo@tegik.com>
2019-04-02 22:13:00 -0600

    cambios a profile

--- src/app/+profile/profile.component.ts
+++ src/app/+profile/profile.component.ts
@@ -5,7 +5,7 @@ import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask 
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthService } from '../core/services/auth.service';
 import { UserInterface } from '../core/models/user';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { UsuarioService } from '../core/services/usuario.service';
 
 @Component({
@@ -20,15 +20,15 @@ export class ProfileComponent implements OnInit {
   task: AngularFireUploadTask;
 
   public user: UserInterface;
-  downloadURL: Observable<string>;
-uploadProgress: Observable<number>;
+  downloadURL: Observable < string > ;
+  uploadProgress: Observable < number > ;
 
 
 
   constructor(private afStorage: AngularFireStorage,
     private authService: AuthService,
-    private afsAuth: AngularFireAuth, 
-    private userService:UsuarioService) {
+    private afsAuth: AngularFireAuth,
+    private userService: UsuarioService) {
     this.authService.getUser().subscribe(user => {
       this.user = user;
     });
@@ -36,16 +36,16 @@ uploadProgress: Observable<number>;
 
   ngOnInit() {}
 
-upload(event) {
+  upload(event) {
     const id = Math.random().toString(36).substring(2);
     this.ref = this.afStorage.ref(id);
     this.task = this.ref.put(event.target.files[0]);
     this.uploadProgress = this.task.percentageChanges();
-    this.uploadProgress.subscribe(item=>{
-      if(item == 100){
-        this.ref.getDownloadURL().subscribe(url=>{
-        this.user.img_profile= url;
-        this.userService.update(this.user, );
+    this.uploadProgress.subscribe(item => {
+      if (item == 100) {
+        this.ref.getDownloadURL().subscribe(url => {
+          this.user.img_profile = url;
+          this.userService.update(this.user, );
 
         });
       }
